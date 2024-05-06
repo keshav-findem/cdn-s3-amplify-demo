f6e28d9a16732c34edd084c6027c8b2601955741
keshav-findem <166017332+keshav-findem@users.noreply.github.com>
2024-05-06 16:00:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Fix to be deployed in preprod first. Waiting QA verification..
         </p>
         <a
           className="App-link"
